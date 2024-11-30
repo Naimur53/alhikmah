f9742abb5e6741afb1db509d2fe002bedaa60166
naimur53 <naimurrhman53@gmail.com>
2024-12-01 05:03:36 +0600

    deo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,15 @@ export default function Home() {
   return (
     <>
       <div className="text-red-500">Hello From Home page</div>
+      <Home></Home>
+      add every thing on home on home folder crate all section like <br />
+      /src/components/home/Home.tsx
+      <br />
+      /src/components/home/Banner.tsx
+      <br />
+      /src/components/home/HomeFirstSlider.tsx
+      <br />
+      /src/components/home/HomeFirstSlider.tsx
     </>
   );
 }
