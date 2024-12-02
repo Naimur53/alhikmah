68e5caa1c7ef3a286fd656e8b2d3e50b2b394225
naimur53 <naimurrhman53@gmail.com>
2024-12-02 22:59:07 +0600

    deo

--- src/componets/Bukhari/Bukhari.jsx
+++ src/componets/Bukhari/Bukhari.jsx
@@ -3,20 +3,20 @@ import React from "react";
 function Bukhari() {
   return (
     <div
-      className="container h-[355px] flex items-center justify-center bg-cover bg-center bg-no-repeat my-[50px] md:my-[140px]"
+      className="container md:h-[355px] bg-[length:100%_160px] h-[200px] flex items-center md:bg-contain justify-center   bg-no-repeat my-[50px] md:my-[140px]"
       style={{
         backgroundImage: `url('/images/bg.jpg')`,
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
+        // backgroundSize: "10px",
+        backgroundPosition: "center"
       }}
     >
       {/* Content */}
-      <div className="text-center px-4 lg:max-w-[1050px] text-black">
-        <p className="text-[20px] sm:text-[24px] md:text-[28px] lg:text-[30px] leading-[30px] sm:leading-[36px] md:leading-[42px] text-[#3B4856] font-medium">
-          কসম খোদার! তোমার মাধ্যমে একজন মানুষের হেদায়েত হওয়াটা তোমার জন্য অনেকগুলি 
-          ‘লাল উটের’ মালিক হওয়ার চেও অধিক সৌভাগ্যের বিষয়।
+      <div className="text-center px-4 max-w-[90%]  xl:max-w-[1050px] text-black">
+        <p className="text-[14px]  md:text-[20px] leading-[20px] xl:text-[30px] xl:leading-[42px] md:leading-[30px] text-[#3B4856] font-medium">
+          কসম খোদার! তোমার মাধ্যমে একজন মানুষের হেদায়েত হওয়াটা তোমার জন্য
+          অনেকগুলি ‘লাল উটের’ মালিক হওয়ার চেও অধিক সৌভাগ্যের বিষয়।
         </p>
-        <h3 className="text-[18px] sm:text-[20px] md:text-[24px] lg:text-[30px] leading-[30px] sm:leading-[36px] md:leading-[42px] text-[#3B4856] font-bold mt-4">
+        <h3 className="text-[12px] sm:text-[20px] md:text-[24px] lg:text-[30px] leading-[20px] sm:leading-[36px] md:leading-[42px] text-[#3B4856] font-bold mt-2 md:mt-4">
           বুখারী, ২৯৪২
         </h3>
       </div>
